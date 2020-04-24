4617dcfc0c5bf8edf27b17c7802dc94b471597d4
jsantore <jsantore@bridgew.edu>
2020-04-24 11:23:05 -0400

    very first pass at nudging Jianan in the right direction

--- 4.17.py
+++
@@ -1,3 +0,0 @@
-# Jianan Hao
-# April 17th 2020
-# COMP490 Honors Contract
---
+++ DataRetreival.py
@@ -0,0 +1,20 @@
+# Jianan Hao
+# April 17th 2020
+# COMP490 Honors Contract
+
+def get_data(location:str):
+    pass
+    #Jianan to fill in here - first get the data and then return it
+
+def print_data(data):
+    pass
+    #Jianan to fill in here- step through the data and print it
+    # this is temporary but useful to make sure get_data worked
+
+def main():
+    api_location = ""#Jianan replace the "" with the actual address
+    returned_data = get_data()
+    print_data(returned_data)
+
+if __name__ == '__main__':
+    main()
